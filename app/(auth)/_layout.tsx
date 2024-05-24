ba81b8bd974588d4d90c054d205bf2215e5d18bf
Larry Reaux <larryreaux@gmail.com>
2024-05-23 19:21:40 -0500

    setup file paths

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  );
+};
+
+export default AuthLayout;
