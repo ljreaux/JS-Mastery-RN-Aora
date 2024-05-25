e7e8253052fedced01a3192eab6fb7319e5a1792
ljreaux <146141261+ljreaux@users.noreply.github.com>
2024-05-24 20:38:36 -0500

    finished sign-in/up screens

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,16 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+    <>
+      <Stack>
+        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      </Stack>
+      <StatusBar backgroundColor="#161622" style="light"></StatusBar>
+    </>
   );
 };
 
